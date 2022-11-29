2fd17cff1ad3a0bdb462563c0391befd443bd582
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2022-11-29 11:52:08 +0800

    Adjusted for direct speaking

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,30 +7,36 @@ import os
 import shutil
 
 
+
 def index_page(request):
     print("hi")
     if request.method == "POST":
-        letters = string.ascii_lowercase
+        # letters = string.ascii_lowercase
 
-        file_name = f"{''.join(random.choice(letters) for i in range(10))}.mp3"
+        # file_name = f"{''.join(random.choice(letters) for i in range(10))}.mp3"
 
         text = request.POST['text']
         tdl = request.POST['tdl']
         lang = request.POST['lang']
 
         tts = gTTS(text, lang=lang, tld=tdl)
-        tts.save(file_name)
+        # tts.save(file_name)
+        tts.save('okay.mp3')
 
-        dir = os.getcwd()
-        full_dir = os.path.join(dir, file_name)
+        os.system("mpg321 okay.mp3")
+        # dir = os.getcwd()
+        # full_dir = os.path.join(dir, file_name)
     
-        print(dir)
-        print(full_dir)
+        # print(dir)
+        # print(full_dir)
 
-        dest = shutil.move(full_dir, os.path.join(
-            dir, "mainapp/static/sound_file"))
+        # dest = shutil.move(full_dir, os.path.join(
+            # dir, "mainapp/static/sound_file"))
 
-        data = {"loc" :file_name}
-        return render(request,'download.html',data)
-
-    return render(request, 'index.html')
+        # data = {"loc" :file_name}
+        # return render(request,'download.html',data)
+    
+        return render(request, 'index.html', {'loc':text})
+    return render(request, 'index.html', {'loc':text})
+    # return None
+    
--- try.py
+++ try.py
@@ -24,39 +24,65 @@
 # frame.mainloop()
 
 # python3
-import threading
+# import threading
 
-import pyautogui, sys
-print('Press Ctrl-C to quit.')
-try:
+# import pyautogui, sys
+# print('Press Ctrl-C to quit.')
+# try:
     
-    while True :
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#     while True :
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
     
-        while x > 300:
+#         while x > 300:
             
-            print(positionStr, end='')
-            print('\b' * len(positionStr), end='', flush=True)
-        while x < 300:
-            # x, y = pyautogui.position()
-            # positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#             print(positionStr, end='')
+#             print('\b' * len(positionStr), end='', flush=True)
+#         while x < 300:
+#             # x, y = pyautogui.position()
+#             # positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
 
-            def setInterval(func,time):
-                e = threading.Event()
-                while not e.wait(time):
-                    func()
-            def foo():
-                print('you need to go back')
-            setInterval(foo,5)
+#             def setInterval(func,time):
+#                 e = threading.Event()
+#                 while not e.wait(time):
+#                     func()
+#             def foo():
+#                 print('you need to go back')
+#             setInterval(foo,5)
 
-        # else:
-            # def setInterval(func,time):
-            #     e = threading.Event()
-            #     while not e.wait(time):
-            #         func()
-            # def foo():
-            #     print('you need to go back')
-            # setInterval(foo,5)
-except KeyboardInterrupt:
-    print('\n')
+#         # else:
+#             # def setInterval(func,time):
+#             #     e = threading.Event()
+#             #     while not e.wait(time):
+#             #         func()
+#             # def foo():
+#             #     print('you need to go back')
+#             # setInterval(foo,5)
+# except KeyboardInterrupt:
+#     print('\n')
+
+# Import the required module for text 
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can 
+# play the converted audio
+import os
+
+# The text that you want to convert to audio
+mytext = 'Welcome to geeksforgeeks!'
+
+# Language in which you want to convert
+language = 'en'
+
+# Passing the text and language to the engine, 
+# here we have marked slow=False. Which tells 
+# the module that the converted audio should 
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+# Saving the converted audio in a mp3 file named
+# welcome 
+myobj.save("welcome.mp3")
+
+# Playing the converted file
+os.system("mpg321 welcome.mp3")
